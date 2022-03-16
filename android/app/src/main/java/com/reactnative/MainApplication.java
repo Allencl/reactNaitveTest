3358a1e332d8276cae1211c573c582372327e777
Allencl <Allen__cc@163.com>
2022-03-16 15:59:50 +0800

    1

--- android/app/src/main/java/com/reactnative/MainApplication.java
+++ android/app/src/main/java/com/reactnative/MainApplication.java
@@ -1,9 +1,11 @@
 package com.reactnative;
 
+
 import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import org.reactnative.camera.RNCameraPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -12,9 +14,42 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
 
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContext;  
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.ReactActivity;
+
+import android.content.BroadcastReceiver;
+import android.content.Intent;
+import android.content.IntentFilter;
+
+import android.widget.Toast; 
+import com.facebook.react.modules.core.DeviceEventManagerModule;
+
+
 import com.reactnative.CustomToastPackage; // <-- 引入你自己的包
 
 public class MainApplication extends Application implements ReactApplication {
+
+
+  private static final String INTENT_ACTION_SCAN_RESULT="com.honeywell.ezservice.uniqueaction";
+  private void sendEvent(ReactContext reactContext,String abc) {
+    reactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit("globalEmitter_honeyWell", abc);
+  }
+
+  private BroadcastReceiver barcodeReceiver = new BroadcastReceiver() {
+      @Override
+      public void onReceive(Context context, Intent intent) {
+          if (INTENT_ACTION_SCAN_RESULT.equals(intent.getAction())) {
+              //获取扫描数据，并将扫描数据存放在barcodeData中
+              final String barcodeData = intent.getStringExtra("data");
+
+              // Toast.makeText( getApplicationContext(),barcodeData,Toast.LENGTH_SHORT ).show();
+              ReactContext reactContext = getReactNativeHost().getReactInstanceManager().getCurrentReactContext();
+              sendEvent(reactContext,barcodeData);
+            }
+      }
+  };
 
   private final ReactNativeHost mReactNativeHost =
       new ReactNativeHost(this) {
@@ -49,6 +84,9 @@ public class MainApplication extends Application implements ReactApplication {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
     initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
+
+    registerReceiver(barcodeReceiver, new IntentFilter(INTENT_ACTION_SCAN_RESULT));
+
   }
 
   /**
@@ -66,7 +104,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.reactnative.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.mesapp.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
